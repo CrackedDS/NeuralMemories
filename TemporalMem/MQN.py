1d887dfc2451305bb5ac9c94d449a29e29c301ec
Deep-Sek <deepan.sek@gmail.com>
2017-11-02 16:18:29 -0400

    Several Changes

---
+++ TemporalMem/MQN.py
@@ -0,0 +1,172 @@
+import sys, os
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from keras.models import load_model
+from keras.optimizers import RMSprop
+from keras.utils import plot_model
+import keras.backend as K
+
+from .rl.agents.dqn import DQNAgent
+from .rl.policy import EpsGreedyQPolicy, LinearAnnealedPolicy
+from .rl.memory import SequentialMemory, Memory
+from .rl.callbacks import TrainEpisodeLogger
+
+import gym
+from .openai_maze_envs import gym_maze
+from .MQNModel import MQNmodel
+
+
+# Need to update environment to have indicator tiles
+# Need better metrics (percent of episodes)
+# Need model saving / loading
+# Need model visualization ability
+
+def visualizeLayer(model, layer, sample):
+    inputs = [K.learning_phase()] + model.inputs
+
+    _output = K.function(inputs, [layer.output])
+
+    def output(x):
+        return _output([0] + [x])
+
+    output = output(sample)
+    output = np.squeeze(output)
+
+    print(output.shape)
+    plt.imshow(output[0], cmap='jet')
+    plt.show()
+
+    print(output)
+    print("Output shape: ", output.shape)
+
+
+def showmetrics(log):
+    ##### Metrics #####
+    episodic_reward_means = []
+    episodic_losses = []
+    episodic_mean_q = []
+
+    for key, value in log.rewards.items():
+        episodic_reward_means.append(np.mean(log.rewards[key]))
+
+    for key, value in log.episodic_metrics_variables.items():
+        for i in range(0, len(log.episodic_metrics_variables[key]), 2):
+            name = log.episodic_metrics_variables[key][i]
+            val = log.episodic_metrics_variables[key][i + 1]
+            if (name == "loss" and val != '--'):
+                episodic_losses.append(val)
+            if (name == "mean_q" and val != '--'):
+                episodic_mean_q.append(val)
+
+                # Running average
+                # episodic_reward_means = (np.cumsum(episodic_reward_means)
+                # / (np.arange(len(episodic_reward_means)) + 1))
+
+    plt.figure(1)
+    plt.subplot(311)
+    plt.title("Loss per Episode")
+    plt.plot(episodic_losses, 'b')
+
+    plt.subplot(312)
+    plt.title("Mean Q Value per Episode")
+    plt.plot(episodic_mean_q, 'r')
+
+    plt.subplot(313)
+    plt.title("Reward")
+    plt.plot(episodic_reward_means, 'g')
+    plt.show()
+
+
+def main(weights_file):
+    # Initialize maze environments.
+    env = gym.make('MazeF4-v0')
+    env2 = gym.make('MazeF1-v0')
+    env3 = gym.make('MazeF2-v0')
+    env0 = gym.make('MazeF3-v0')
+    env5 = gym.make('Maze5-v0')
+    env6 = gym.make('BMaze4-v0')
+
+    envs = [env, env2, env3, env4, env5, env6]
+
+    # Setting hyperparameters.
+    nb_actions = env.action_space.n
+    e_t_size = 16
+    context_size = 16
+    nb_steps_warmup = int(2e4)
+    nb_steps = int(1e5)
+    buffer_size = 6e6
+    learning_rate = 1e-4
+    target_model_update = 0.999
+    clipnorm = 20.
+    switch_rate = 1000
+
+    # Callbacks
+    log = TrainEpisodeLogger()
+    callbacks = [log]
+
+    # Initialize our model.
+    model = MQNmodel(e_t_size, context_size, nb_actions)
+    target_model = MQNmodel(e_t_size, context_size, nb_actions)
+    target_model.set_weights(model.get_weights())
+
+    # Initialize memory buffer and policy for DQN algorithm.
+    experience = [SequentialMemory(limit=int(buffer_size / len(envs)), window_length=1) for i in range(len(envs))]
+
+    policy = LinearAnnealedPolicy(
+        inner_policy=EpsGreedyQPolicy(),
+        attr="eps",
+        value_max=1.0,
+        value_min=0.1,
+        value_test=0.,
+        nb_steps=nb_steps_warmup
+    )
+
+    # Initialize and compile the DQN agent.
+    dqn = DQNAgent(model=model, target_model=target_model, nb_actions=nb_actions, memory=experience,
+                   nb_steps_warmup=nb_steps_warmup, target_model_update=target_model_update, policy=policy)
+
+    dqn.compile(RMSprop(lr=learning_rate, clipnorm=clipnorm), metrics=["mae"])
+    observation = env.reset()
+    observation = np.reshape(observation, ((1, 1, 6, 7)))
+    print
+    observation
+    print
+    dqn.layers[1]
+    visualizeLayer(dqn.model, dqn.layers[1], observation)
+    # Load weights if weight file exists.
+    '''
+    if os.path.exists(weights_file):
+        dqn.load_weights(weights_file)
+    #Train DQN in environment.
+    dqn.fit(env, envs=envs, switch_rate=switch_rate, nb_steps=nb_steps, visualize=True, verbose=0, callbacks=callbacks)
+    #Save weights if weight file does not exist.
+    if not os.path.exists(weights_file):
+        dqn.save_weights(weights_file)
+    '''
+    # Visualization Tools
+    showmetrics(log)
+
+    return
+
+
+if __name__ == "__main__":
+
+    weights_file = None
+
+    if len(sys.argv) == 2:
+        if (sys.argv[1].split('.')[-1] == "h5"):
+            weights_file = sys.argv[1]
+        else:
+            print
+            "File extension must be .h5"
+            sys.exit()
+    else:
+        print
+        "Incorrect number of arguments."
+        print
+        "Usage: python MQN.py [weight_filename].h5"
+        sys.exit()
+
+    main(weights_file)
--- TemporalMem/TemporalMemory.py
+++ TemporalMem/TemporalMemory.py
@@ -8,12 +8,14 @@ from keras import backend as K
 from keras import initializers
 from keras import regularizers
 from keras import constraints
-from keras.engine import Layer
+from keras.engine.topology import Layer
 from keras.layers.recurrent import RNN
 
 # Legacy support.
 from keras.legacy.layers import Recurrent
 from keras.legacy import interfaces
+
+import tensorflow as tf
 
 
 class SimpleMemoryCell(Layer):
@@ -65,15 +67,15 @@ class SimpleMemoryCell(Layer):
         self.recurrent_constraint = constraints.get(recurrent_constraint)
 
         self.memory_size = memory_size
-        self.state_size = (self.units, ) * (self.memory_size * 2) # M_key, M_value
+        self.state_size = (self.units,) * (self.memory_size * 2)  # M_key, M_value
 
     def build(self, input_shape):
         input_dim = input_shape[-1]
         self.W_key = self.add_weight(shape=(input_dim - self.units, self.units),
-                                      name='W_key',
-                                      initializer=self.kernel_initializer,
-                                      regularizer=self.kernel_regularizer,
-                                      constraint=self.kernel_constraint)
+                                     name='W_key',
+                                     initializer=self.kernel_initializer,
+                                     regularizer=self.kernel_regularizer,
+                                     constraint=self.kernel_constraint)
         self.W_value = self.add_weight(
             shape=(input_dim - self.units, self.units),
             name='W_value',
@@ -85,22 +87,34 @@ class SimpleMemoryCell(Layer):
 
     def call(self, inputs, states, training=None):
         # inputs is e_t concatenated with h_t
-        e_t = inputs[:-self.units] # shape=[1xe]
-        h_t = inputs[-self.units:] # shape=[1xm]
+
+        # e_t = inputs[:-self.units] # shape=[1xe]
+        # h_t = inputs[-self.units:] # shape=[1xm]
+        e_t = inputs[:, :self.units]
+        h_t = inputs[:, self.units:]
 
         # states is M_key, M_value
-        M_key = states[:self.memory_size] # shape=[Mxm]
-        M_value = states[self.memory_size:] # shape=[Mxm]
 
+        M_key = list(states[:self.memory_size])  # shape=[Mxm]
+        M_value = list(states[self.memory_size:])  # shape=[Mxm]
+
+        # Conversion to tensors
+        M_key_tens = K.concatenate(M_key, axis=1)
+        M_key_tens = K.reshape(M_key_tens, (-1, self.memory_size, self.units))
+
+        M_value_tens = K.concatenate(M_value, axis=1)
+        M_value_tens = K.reshape(M_value_tens, (-1, self.memory_size, self.units))
+
+        # print M_key_tens
+        # print K.transpose(h_t)
         # calculate attention probability
+        '''
         at = K.exp(K.dot(M_key, K.transpose(h_t)))
         at_sum = K.sum(at, axis=1)
         at_sum_repeated = K.repeat(at_sum, self.memory_size)
         at /= at_sum_repeated # shape = [Mx1]
-
         # calculate output from attention probability
         output = K.dot(K.transpose(at), M_value) # shape = [1xm]
-
         #update states
         M_key.pop(0) # shape = [M-1xm]
         M_value.pop(0) # shape = [M-1xm]
@@ -108,8 +122,40 @@ class SimpleMemoryCell(Layer):
         m_value = K.dot(e_t, self.W_value) # shape = [1xm]
         M_key.append(m_key) # shape = [Mxm]
         M_value.append(m_value) # shape = [Mxm]
+        '''
+        at = K.exp(K.batch_dot(M_key_tens, h_t, axes=[2, 1]))
 
-        return output, [M_key, M_value]
+        # at = K.exp(K.dot(M_key_tens, K.transpose(h_t)))
+        at_sum = K.sum(at, axis=1)
+        at_sum = K.reshape(at_sum, (-1, 1))
+        at_sum_repeated = K.repeat(at_sum, self.memory_size)
+        at_sum_repeated = K.reshape(at_sum_repeated, (-1, self.memory_size))
+        at /= at_sum_repeated  # shape = [Mx1]
+
+        # calculate output from attention probability
+        # output = K.dot(K.transpose(at), M_value_tens[0, :, :]) # shape = [1xm]
+        # output = K.dot(K.transpose(at), M_value_tens)
+
+        output = K.batch_dot(M_value_tens, at, axes=[1, 1])
+        print(output)
+        # update states
+        M_key.pop(0)  # shape = [M-1xm]
+        M_value.pop(0)  # shape = [M-1xm]
+        m_key = K.dot(e_t, self.W_key)  # shape = [1xm]
+        m_value = K.dot(e_t, self.W_value)  # shape = [1xm]
+        M_key.append(m_key)  # shape = [Mxm]
+        M_value.append(m_value)  # shape = [Mxm]
+
+        return output, M_key + M_value
+
+
+'''
+    def get_config(self):
+        config={'memory_size' : self.memory_size}
+        base_config = super(SimpleMemoryCell, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+'''
+
 
 class SimpleMemory(RNN):
     """Long-Short Term Memory layer - Hochreiter 1997.
@@ -149,20 +195,20 @@ class SimpleMemory(RNN):
                  memory_size=10,
                  **kwargs):
         cell = SimpleMemoryCell(units,
-                        kernel_initializer=kernel_initializer,
-                        recurrent_initializer=recurrent_initializer,
-                        kernel_regularizer=kernel_regularizer,
-                        recurrent_regularizer=recurrent_regularizer,
-                        kernel_constraint=kernel_constraint,
-                        recurrent_constraint=recurrent_constraint,
-                        memory_size=memory_size)
-        super(SimpleMemory, self).__init__(cell, **kwargs)
+                                kernel_initializer=kernel_initializer,
+                                recurrent_initializer=recurrent_initializer,
+                                kernel_regularizer=kernel_regularizer,
+                                recurrent_regularizer=recurrent_regularizer,
+                                kernel_constraint=kernel_constraint,
+                                recurrent_constraint=recurrent_constraint,
+                                memory_size=memory_size)
+        super(SimpleMemory, self).__init__(cell=cell, **kwargs)
 
     def call(self, inputs, mask=None, training=None, initial_state=None):
         return super(SimpleMemory, self).call(inputs,
-                                      mask=mask,
-                                      training=training,
-                                      initial_state=initial_state)
+                                              mask=mask,
+                                              training=training,
+                                              initial_state=initial_state)
 
     @property
     def units(self):
@@ -205,6 +251,8 @@ class SimpleMemory(RNN):
                   'kernel_constraint': constraints.serialize(self.kernel_constraint),
                   'recurrent_constraint': constraints.serialize(self.recurrent_constraint),
                   'memory_size': self.memory_size}
+
         base_config = super(SimpleMemory, self).get_config()
+
         del base_config['cell']
         return dict(list(base_config.items()) + list(config.items()))
